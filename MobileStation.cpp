88c9d6be769ebfa512161a54510af438ab5c48b2
krzydyn <krzydyn@gmail.com>
2018-04-05 02:17:39 +0200

    ARFCN calc

--- MobileStation.cpp
+++ MobileStation.cpp
@@ -19,25 +19,42 @@ GSM1800 512..885    1720.2 + 0.2*(n-512)    f(ul)+95.0
 GSM1900 512..810    1850.2 + 0.2*(n-512)    f(ul)+80.0
 */
 // http://www.rfwireless-world.com/Terminology/GSM-ARFCN-to-frequency-conversion.html
+// http://www.rfwireless-world.com/Tutorials/gsm-frame-structure.html
 struct {
 	GsmBand band;
 	int first, last;
 	double base_freq;
 	double dnl_offs;
-	int fc_offs;
 } band_channels[] = {
-	{GsmBand::GSM450,  259,  293,  450.6, 10.0, -259},
-	{GsmBand::GSM480,  306,  340,  479.0, 10.0, -306},
-	{GsmBand::GSM750,  348,  511,  747.2, 30.0, -438},
-	{GsmBand::GSM850,  128,  251,  824.2, 30.0, -128},
+	{GsmBand::GSM450,  259,  293,  450.6-259*0.2, 10.0},
+	{GsmBand::GSM480,  306,  340,  479.0-306*0.2, 10.0},
+	{GsmBand::GSM750,  438,  511,  747.2-438*0.2, 30.0},
+	{GsmBand::GSM850,  128,  251,  824.2-128*0.2, 30.0},
 
-	{GsmBand::GSM900,    0,  124,  890.0, 45.0, 0},
-	{GsmBand::GSM900,  955, 1023,  890.0, 45.0, -1024},
-	{GsmBand::GSM1800, 512,  885, 1710.2, 95.0, -512},
-	{GsmBand::GSM1900, 512,  810, 1850.2, 80.0, -512},
+	{GsmBand::GSM900,    0,  124,  890.0, 45.0}, //p-gsm
+	{GsmBand::GSM900,  955, 1023,  890.0-1024*0.2, 45.0}, //gsm-r(e-gsm)
+	{GsmBand::GSM1800, 512,  885, 1710.2-512*0.2, 95.0}, //dcs
+	{GsmBand::GSM1900, 512,  810, 1850.2-512*0.2, 80.0}, //pcs
 
-	{GsmBand::Undef, 0, 0, 0, 0, 0}
+	{GsmBand::Undef, 0, 0, 0, 0}
 };
+
+GsmBand upLinkFreq(int n, double& freq) {
+	for (int i = 0; band_channels[i].band != GsmBand::Undef; ++i) {
+		if (n < band_channels[i].first || n > band_channels[i].last) continue;
+		freq = band_channels[i].base_freq + 0.2*n;
+		return band_channels[i].band;
+	}
+	return GsmBand::Undef;
+}
+GsmBand dnLinkFreq(int n, double& freq) {
+	for (int i = 0; band_channels[i].band != GsmBand::Undef; ++i) {
+		if (n < band_channels[i].first || n > band_channels[i].last) continue;
+		freq = band_channels[i].base_freq + 0.2*n + band_channels[i].dnl_offs;
+		return band_channels[i].band;
+	}
+	return GsmBand::Undef;
+}
 
 void MobileStation::btsScan(GsmBand band) {
 	LOGD("btsScan...");
@@ -47,7 +64,7 @@ void MobileStation::btsScan(GsmBand band) {
 	for (int i = 0; band_channels[i].band != GsmBand::Undef; ++i) {
 		if (band != band_channels[i].band) continue;
 		for (int n = band_channels[i].first; n <= band_channels[i].last; ++n) {
-			double upl = band_channels[i].base_freq + 0.2*(n + band_channels[i].fc_offs);
+			double upl = band_channels[i].base_freq + 0.2*n;
 			double dnl = upl + band_channels[i].dnl_offs; 
 			LOGD("ARFCN = %d,  upl %.2lf, dnl %.2lf", n, upl, dnl);
 			upl *= 1e6;
